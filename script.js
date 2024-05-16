be7325fc2bae99ccc782008ec45da623988baa26
BARATHRAJI P <161221792+BARATHRAJI-2004@users.noreply.github.com>
2024-05-16 18:12:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-let display = document.getElementById('display');
-
-function appendToDisplay(value) {
-    display.value += value;
+.calculator {
+  width: 300px;
+  margin: 0 auto;
+  text-align: center;
 }
 
-function clearDisplay() {
-    display.value = '';
+.display {
+  width: 100%;
+  height: 50px;
+  margin-bottom: 10px;
+  font-size: 24px;
 }
 
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'Error';
-    }
+.buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 5px;
+}
+
+.buttons button {
+  width: 100%;
+  height: 50px;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.history {
+  margin-top: 20px;
+}
+
+.history h3 {
+  margin-bottom: 10px;
+}
+
+#history-list {
+  list-style-type: none;
+  padding: 0;
+}
+
+#history-list li {
+  margin-bottom: 5px;
 }
